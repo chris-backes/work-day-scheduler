7607110d54a6e463050003a68fa599bc441f4286
Christopher Backes <you@example.com>
2021-12-24 19:33:03 -0500

    add save/load tasks function

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ function addDate() {
   dateToday = dateToday.toString().split(" ");
   $("#currentDay").text(
     dateToday[0] +
-      " " +
+      "., " +
       dateToday[1] +
       ". " +
       dateToday[2] +
@@ -21,12 +21,19 @@ function addCalendar() {
     schedulerBoxEl.append(timeRowEl);
     let timeBoxEl = $("<div>").addClass("col-md-1 hour");
     let scheduleEventEl = $("<textarea>")
-      .addClass("col-md-10 text-center border")
+      .addClass("col-md-10 border")
       .attr("placeholder", "Enter Text");
-    let buttonEl = $("<button>").addClass("col-md-1 saveBtn");
-    if (i < 12) timeBoxEl.text(i + " AM");
-    else if (i === 12) timeBoxEl.text(i + " PM");
-    else timeBoxEl.text(i - 12 + " PM");
+    let buttonEl = $("<button>").addClass("col-md-1 oi oi-task saveBtn");
+    if (i < 12) {
+      timeBoxEl.text(i + " AM");
+      timeRowEl.attr("data-hour", i);
+    } else if (i === 12) {
+      timeBoxEl.text(i + " PM");
+      timeRowEl.attr("data-hour", i);
+    } else {
+      timeBoxEl.text(i - 12 + " PM");
+      timeRowEl.attr("data-hour", i - 12);
+    }
     colorCoding(i, scheduleEventEl);
     timeRowEl.append(timeBoxEl, scheduleEventEl, buttonEl);
   }
@@ -42,5 +49,35 @@ function colorCoding(hour, textArea) {
   }
 }
 
+function loadTasks() {
+  for (let i = 9; i <= 17; i++) {
+    if (i <= 12 && localStorage.getItem(i) != null) {
+      let storedTask = localStorage.getItem(i);
+      console.log(storedTask);
+      $("div[data-hour=" + i + "]")
+        .children("textarea")
+        .text(storedTask);
+    } else if (i > 12 && localStorage.getItem(i) != null) {
+      let j = i - 12;
+      let storedTask = localStorage.getItem(j).value;
+      $("div[data-hour=" + j)
+        .child("<textarea>")
+        .text(storedTask);
+    }
+  }
+}
+
 addDate();
 addCalendar();
+
+$(".saveBtn").click(function () {
+  let appointment = $(this).siblings("textarea").val();
+  let hour = $(this).parent().attr("data-hour");
+
+  if (!localStorage.getItem(hour)) {
+    localStorage.setItem(hour, appointment);
+  } else {
+    localStorage.removeItem(hour);
+    localStorage.setItem(hour, appointment);
+  }
+});
